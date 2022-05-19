4f66c2a9a463522254219f0a6f33cb1ae7f1693b
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-05-19 22:13:07 +0700

    Initial commit

---
+++ app/src/main/java/com/refeed_ppb1/animationpapb2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.refeed_ppb1.animationpapb2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorSet;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
